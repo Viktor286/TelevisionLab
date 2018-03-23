9df5a5885a93d0de4e1a884e965bad6d2d32b235
Viktor286 <just.viktor@gmail.com>
2018-03-24 01:56:30 +0300

    env mode add

--- webpack.config.js
+++ webpack.config.js
@@ -4,82 +4,94 @@ const webpack = require('webpack');
 // const ConcatPlugin = require('webpack-concat-plugin');
 const AssetsPlugin = require('assets-webpack-plugin');
 
-console.log('>>>>>>>>>>: '+__dirname);
+const mode = process.env.NODE_ENV;
+
+mode === 'production' ? console.log('production >>>>>>>>>>: '+__dirname) : null;
+
+let moduleDevRules = {
+    rules: [
+        {
+            enforce: "pre",
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'eslint-loader',
+                options: {
+                    // this will edit source files
+                    // fix: true
+                }
+            }
+        },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015'] // es2015, env
+                }
+            }
+        }
+    ],
+};
+
+let pluginDevRules = [
+
+];
+
+
+let moduleProductionRules = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015'] // es2015, env
+                }
+            }
+        }
+    ],
+};
+
+let pluginProductionRules = [
+    new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new AssetsPlugin({
+        filename: 'build.js.json',
+        path: __dirname + '/desktop/js/manifest/'
+    })
+
+];
+
+
+
 module.exports = {
 
     context: __dirname,
 
     entry: {
         build: './desktop/js/static/app.js'
-        // vendors: './desktop/js/static/vendors.js'
     },
 
     output: {
         path: __dirname + '/desktop/js/',
-        filename: '[name].[chunkhash].js'
+        filename: mode === 'production' ? '[name].[chunkhash].js' : '[name].js',
     },
 
-    watch: false,
+    watch: mode !== 'production',
 
     watchOptions: {
         aggregateTimeout: 100
     },
 
-    devtool: 'source-map',
+    devtool: mode === 'production' ? "source-map" : false,
 
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-        }),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new AssetsPlugin({
-            filename: 'build.js.json',
-            path: __dirname + '/desktop/js/manifest/'
-        })
+    plugins: mode === 'production' ? pluginProductionRules : pluginDevRules,
 
-    ]
-
-    // console.log('NODE_ENV: ', env.NODE_ENV); // 'local', --env.NODE_ENV=development
-    // console.log('Production: ', env.production); // true, --env.production
-
-
-        // devtool: env.NODE_ENV === 'development' ? "cheap-inline-module-source-map" : "source-map",
-        // source-map, cheap-inline-module-source-map, eval
-
-
-
-        // watch: env.NODE_ENV === 'development', // watch: true,
-        // watchOptions: {
-        //     aggregateTimeout: 100
-        // }
-
-        // module: {
-        //     rules: [
-        //         {   //* Babel *// npm install babel-loader babel-cli babel-core babel-preset-env babel-preset-es2015 -DE
-        //             test: /\.js$/,
-        //             // include: path.join(__dirname, 'src'),
-        //             exclude: /(node_modules|bower_components)/,
-        //             use: {
-        //                 loader: 'babel-loader',
-        //                 options: {
-        //                     presets: ['env'] // es2015
-        //                 }
-        //             }
-        //         }
-        //
-        //         // {   // * CSS * // npm install style-loader css-loader -DE
-        //         //     test: /\.css$/,
-        //         //     use: ExtractTextPlugin.extract({
-        //         //         fallback: "style-loader",
-        //         //         use: "css-loader"
-        //         //     })
-        //         // }
-        //     ]
-        // },
-
-        // plugins: [
-        //     new ExtractTextPlugin("bundle.css"),
-        // ]
-
+    module: mode === 'production' ? moduleProductionRules : moduleDevRules
 
 };
